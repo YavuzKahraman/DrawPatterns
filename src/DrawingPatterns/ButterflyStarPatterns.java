e2725c5b7a8077f1ca5a3d692abb1f64d17d61f2
YavuzKahraman <yavuzkhrmn@gmail.com>
2024-02-01 16:41:10 +0300

    add butterfly shape with stars

--- src/DrawingPatterns/ButterflyStarPatterns.java
+++ src/DrawingPatterns/ButterflyStarPatterns.java
@@ -1,10 +1,66 @@
 package DrawingPatterns;
 
 public class ButterflyStarPatterns {
+	
+	//Butterfly star pattern
+	/*           *
+	 * *       * *
+	 * * *   * * *
+	 * * * * * * *
+	 * * *   * * *
+	 * *       * *
+	 *           *
+	 */
+	
+	public static void bflyShape(int n) {
+		int i, j;
+ 
+        // outer loop to handle upper part
+        for (i = 1; i <= n; i++) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+ 
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            System.out.println();
+        }
+ 
+        // outer loop to handle lower part
+        for (i = n; i >= 1; i--) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+ 
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            System.out.println();
+        }
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		bflyShape(5);
 	}
 
 }
