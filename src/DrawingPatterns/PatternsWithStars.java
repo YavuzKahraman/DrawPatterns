448988f8c525848ed5a48355e42e6c6caa9304ab
YavuzKahraman <yavuzkhrmn@gmail.com>
2024-02-02 22:38:04 +0300

    pattern changes are updated

--- src/DrawingPatterns/PatternsWithStars.java
+++ src/DrawingPatterns/PatternsWithStars.java
@@ -34,10 +34,52 @@ public class PatternsWithStars {
 			System.out.println();
 		}
 	}
+	
+	/*butterfly shape
+	 * 
+	 *             *
+	 * *         * *
+	 * * *     * * *
+	 * * * * * * * *
+	 * * *     * * *
+	 * *         * *
+	 *             * 
+	 * 
+	 * */
+	
+	public static void butterflywithstars(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= 2*(n-i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = n-1; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= 2*(n-i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		diamondShapewithStars(5);
+		diamondShapewithStars(5);System.out.println();
+		butterflywithstars(5);System.out.println();
 
 	}
 
