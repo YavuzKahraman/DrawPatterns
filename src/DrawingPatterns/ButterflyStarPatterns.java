b2d81f7f29fb36936a52da1f2fce214f052c5685
YavuzKahraman <yavuzkhrmn@gmail.com>
2024-02-02 14:30:22 +0300

    some new pattern shapes are added. e.g. butterfly, reverse triangular neumber etc

--- src/DrawingPatterns/ButterflyStarPatterns.java
+++ src/DrawingPatterns/ButterflyStarPatterns.java
@@ -1,5 +1,7 @@
 package DrawingPatterns;
 
+import java.util.Iterator;
+
 public class ButterflyStarPatterns {
 	
 	//Butterfly star pattern
@@ -77,11 +79,17 @@ public class ButterflyStarPatterns {
 		}
 	}
 	
-	/*right half pyramid
-	 * *
-	 * **
-	 * ***
-	 * ****
+	/*right aligned arrow edge
+	 * 1
+	 * 22
+	 * 333
+	 * 4444
+	 * 55555
+	 * 55555
+	 * 4444
+	 * 333
+	 * 22
+	 * 1
 	 * *****
 	 * */
 	public static void rightHalfPyramid(int n) {
@@ -92,7 +100,7 @@ public class ButterflyStarPatterns {
 			}
 			System.out.println();
 		}
-		System.out.print("****************\n");
+		//System.out.println("****************");
 		for(i=n;i>=1;i--) {
 			for(j=1;j<=i;j++) {
 				System.out.print(i);
@@ -100,11 +108,89 @@ public class ButterflyStarPatterns {
 			System.out.println();
 		}
 	}
+	
+	/*
+	 * 55555
+	 * 4444
+	 * 333
+	 * 22
+	 * 1
+	 * 
+	 * 1
+	 * 12
+	 * 123
+	 * 1234
+	 * 12345
+	 * 
+	 * */
+	
+	public static void leftAlignedDNumbers(int n) {
+		for (int i = n; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	/* right aligned numbers
+	 * . . . 1
+	 * . . 2 2
+	 * . 3 3 3
+	 * 4 4 4 4
+	 * 
+	 * */
+	
+	public static void rightAlignedNumbers(int n) {
+		int i, j, k;
+		for(i=1; i<=n; i++) {
+			for(j=1; j <= n-i;j++) {
+				System.out.print("."+" ");
+			}
+			for (k = 1; k <= i; k++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	/*reverse right aligned numbers
+	 * 5 5 5 5 5
+	 * 4 4 4 4
+	 * 3 3 3
+	 * 2 2
+	 * 1
+	 *   
+	 * */
+	public static void reverserightAlignedNumbers(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j <= n-i; j++) {
+				System.out.print(" "+(n-i)+" ");
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print(" . ");
+			}
+			System.out.println();
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		bflyShape(5);System.out.println();
 		squareFilledStars(5);System.out.println();
 		rightHalfPyramid(5);System.out.println();
+		leftAlignedDNumbers(5);System.out.println();
+		rightAlignedNumbers(5);System.out.println();
+		reverserightAlignedNumbers(5);System.out.println();
 	}
 
 }
