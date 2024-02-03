f8102f0646d7aaa40b892b0fb1cdea8a2739b310
YavuzKahraman <yavuzkhrmn@gmail.com>
2024-02-03 12:06:36 +0300

    patterns with only stars are added

--- src/DrawingPatterns/PatternsWithStars.java
+++ src/DrawingPatterns/PatternsWithStars.java
@@ -75,12 +75,48 @@ public class PatternsWithStars {
 		}
 		
 	}
-
+	/*
+	 * *
+	 * * *
+	 * * * * 
+	 * * * * *
+	 * * * * * */
+	public static void leftAlignedStarPattern(int n) {
+		for (int i = 1; i <=n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+	/* * * * * * *
+	 * * * * * *
+	 * * * * *
+	 * * * *
+	 * * *
+	 * *
+	 */
+	public static void rightAlignedStarPattern(int n) {
+		for (int i = n; i >=1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		diamondShapewithStars(5);System.out.println();
 		butterflywithstars(5);System.out.println();
+		leftAlignedStarPattern(5);System.out.println();
+		rightAlignedStarPattern(5);
+
 
 	}
+
+
 
 }
